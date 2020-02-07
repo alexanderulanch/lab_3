4d597083dc1d862a5d066797fc8aa44d4170c058
Alex Ulanch <alexulanch@engr2-2-70-168-dhcp.int.colorado.edu>
2020-02-07 15:32:37 -0700

    Creating practice.sh file and storing regex_practice.sh

---
+++ practice.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Author : Alex Ulanch
+# Date: 02/07/2020
+# Script follows here:
+echo "Enter a number: "
+read numOne
+echo "Enter a second number: "
+read numTwo
+sum=$(($numOne + $numTwo))
+echo "The sum is : $sum"
+let prod=numOne*numTwo
+echo "The product is: $prod"
